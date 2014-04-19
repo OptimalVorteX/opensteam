11fd396a9ae23760a609820b2fc634adf296a700
Grief-Code <hasgrief@googlemail.com>
2014-04-19 18:37:54 +0100

    Improved search field a little bit

--- WEB/themes/default/menu.php
+++ WEB/themes/default/menu.php
@@ -23,8 +23,12 @@
                </li>
             </ul>
             <form class="navbar-form navbar-left" action="" method="get">
-               <input onkeydown="if (event.keyCode == 13) { location.href='<?=OSS_HOME?>?option=<?php if(isset($_GET["option"]) ) echo $_GET["option"]; ?>&search='+srch.value; return false; }" type="text" id="srch" class="form-control col-lg-8" placeholder="Search" value="<?=$search ?>" />
-               <input type="button" onclick="location.href='<?=OSS_HOME?>?option=<?php if(isset($_GET["option"]) ) echo $_GET["option"]; ?>&search='+srch.value" value="Search Player" class="btn btn-primary" />
+              <div class="input-group" style="width: 350px">
+                <input onkeydown="if (event.keyCode == 13) { location.href='<?=OSS_HOME?>?option=<?php if(isset($_GET["option"]) ) echo $_GET["option"]; ?>&search='+srch.value; return false; }" type="text" id="srch" class="form-control col-lg-8" placeholder="Search" value="<?=$search ?>" />
+                <span class="input-group-btn">
+                  <input type="button" onclick="location.href='<?=OSS_HOME?>?option=<?php if(isset($_GET["option"]) ) echo $_GET["option"]; ?>&search='+srch.value" value="Search Player" class="btn btn-primary" />
+                </span>
+              </div>
             </form>
             <ul class="nav navbar-nav navbar-right">
                <?php if(!isset($_SESSION["steamID"])) { ?>
