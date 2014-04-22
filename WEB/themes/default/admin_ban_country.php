085e4ca98a605a0428869916c28d447877e5223c
Ivan <ivan.anta@gmail.com>
2014-04-22 17:30:59 +0200

    Added help for geoIP install
    help for geoIP install

--- WEB/themes/default/admin_ban_country.php
+++ WEB/themes/default/admin_ban_country.php
@@ -2,7 +2,12 @@
 
 <div class="well bs-component">
  
-    <h2><?=$lang["BanCountry"]?></h2>
+    <h2><?=$lang["BanCountry"]?> <a href="javascript:;" onclick="showhide('geoip_info')" >Info?</a></h2>
+   <div class="alert alert-dismissable alert-info">
+   <button type="button" class="close" data-dismiss="alert">&times;</button>
+   <strong>GeoIP Install!</strong> Go to <a href="http://dev.maxmind.com/geoip/legacy/geolite/" target="_blank" class="alert-link">Geoip MaxMind</a> website and download <strong>GeoIPCountryCSV.zip</strong>
+   <div>Unpack zip archive to <strong>/garrysmod/data</strong> folder</div>
+   </div>
 	<form action="" method="post">
 	<?php foreach ($AllCountries as $c) { ?>
 	<div>
